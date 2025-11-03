8f11688e91eeeff9519309174c09a1512604018d
acehighboss <156999331+acehighboss@users.noreply.github.com>
2025-11-03 14:25:29 +0900

    Add ELB and ASG module calls in main.tf

--- 21_quiz/dev/main.tf
+++ 21_quiz/dev/main.tf
@@ -32,6 +32,24 @@ module "instances" {
   vpc_id             = module.networks.vpc_id
 }
 
+# [추가] ELB 모듈 호출
+module "elb" {
+  source            = "../elb"
+  env               = local.env
+  vpc_id            = module.networks.vpc_id
+  public_subnet_ids = module.networks.public_subnet_ids
+}
+
+# [추가] ASG 모듈 호출
+module "asg" {
+  source                = "../asg"
+  env                   = local.env
+  ami_id                = module.instances.ami_id                 # instances 모듈 출력
+  web_sg_id             = module.instances.web_sg_id              # instances 모듈 출력
+  lb_target_group_arn   = module.elb.lb_target_group_arn        # elb 모듈 출력
+  private_subnet_ids    = module.networks.private_subnet_ids      # networks 모듈 출력
+}
+
 # PS C:\terraform\workspace\20_quiz\stage> cd ..\..\21_quiz\dev\
 # PS C:\terraform\workspace\21_quiz\dev> terraform fmt
 # main.tf
